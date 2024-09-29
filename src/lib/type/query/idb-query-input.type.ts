5dff54f4a00bd3e2b0a997ae632df9915d8e7aeb
Scibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-09-29 02:11:06 +0000

    refactor(type IDBQueryInput): add `openCursor` to method.

--- src/lib/type/query/idb-query-input.type.ts
+++ src/lib/type/query/idb-query-input.type.ts
@@ -1,7 +1,8 @@
 // Type.
 import { IDBQueryAdd } from "./idb-query-add.type";
-import { IDBQueryGetAll } from "./idb-query-get-all.type";
 import { IDBQueryGet } from "./idb-query-get.type";
+import { IDBQueryGetAll } from "./idb-query-get-all.type";
+import { IDBQueryOpenCursor } from "./idb-query-open-cursor.type";
 import { IDBQueryPut } from "./idb-query-put.type";
 
 /**
@@ -14,7 +15,8 @@ export type IDBQueryInput<
   add?: Partial<{ [Key in StoreName]: IDBQueryAdd<Key, StoreSchema, StoreName> }>,
   get?: Partial<{ [Key in StoreName]: IDBQueryGet<StoreName> }>,
   getAll?: Partial<{ [Key in StoreName]: IDBQueryGetAll<StoreName> }>,
-  put?: Partial<{ [Key in StoreName]: IDBQueryPut<Key, StoreSchema, StoreName> }>
+  openCursor?: Partial<{ [Key in StoreName]: IDBQueryOpenCursor<StoreName> }>,
+  put?: Partial<{ [Key in StoreName]: IDBQueryPut<Key, StoreSchema, StoreName> }>,
 }
 
 // Examples.
